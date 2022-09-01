0644d2b4ef8a887ec2f8cfdd32a9c37ad6b63581
Jim Applegate <jimappleg8@gmail.com>
2022-09-01 17:42:39 +0000

    Removing some info.

--- system/config/database.php
+++ system/config/database.php
@@ -41,7 +41,7 @@
 
 $db['production']['hostname'] = "mysql-serv-master:3307";
 $db['production']['username'] = "brewuser";
-$db['production']['password'] = "fr33tyr8";
+$db['production']['password'] = "xxxxxx";
 $db['production']['database'] = "coolbrew_live";
 $db['production']['dbdriver'] = "mysql";
 $db['production']['dbprefix'] = "";
@@ -58,7 +58,7 @@ $db['production']['dbcollat'] = "utf8_general_ci";
 
 $db['live-read']['hostname'] = "mysql-serv-master:3307";
 $db['live-read']['username'] = "brewuser";
-$db['live-read']['password'] = "fr33tyr8";
+$db['live-read']['password'] = "xxxxxx";
 $db['live-read']['database'] = "coolbrew_live";
 $db['live-read']['dbdriver'] = "mysql";
 $db['live-read']['dbprefix'] = "";
@@ -72,7 +72,7 @@ $db['live-read']['dbcollat'] = "utf8_general_ci";
 
 $db['live-write']['hostname'] = "mysql-serv-master:3307";
 $db['live-write']['username'] = "brewuser";
-$db['live-write']['password'] = "fr33tyr8";
+$db['live-write']['password'] = "xxxxxx";
 $db['live-write']['database'] = "coolbrew_live";
 $db['live-write']['dbdriver'] = "mysql";
 $db['live-write']['dbprefix'] = "";
@@ -83,41 +83,13 @@ $db['live-write']['cache_on'] = FALSE;
 $db['live-write']['cachedir'] = "";
 $db['live-write']['char_set'] = "utf8";
 $db['live-write']['dbcollat'] = "utf8_general_ci";
-   
-// $db['live-hcg_read']['hostname'] = "mysql-serv-master:3307";
-// $db['live-hcg_read']['username'] = "public_db";
-// $db['live-hcg_read']['password'] = "jage0katt";
-// $db['live-hcg_read']['database'] = "hcgpublic_live";
-// $db['live-hcg_read']['dbdriver'] = "mysql";
-// $db['live-hcg_read']['dbprefix'] = "";
-// $db['live-hcg_read']['active_r'] = TRUE;
-// $db['live-hcg_read']['pconnect'] = TRUE;
-// $db['live-hcg_read']['db_debug'] = TRUE;
-// $db['live-hcg_read']['cache_on'] = FALSE;
-// $db['live-hcg_read']['cachedir'] = "";
-// $db['live-hcg_read']['char_set'] = "utf8";
-// $db['live-hcg_read']['dbcollat'] = "utf8_general_ci";
-// 
-// $db['live-hcg_write']['hostname'] = "mysql-serv-master:3307";
-// $db['live-hcg_write']['username'] = "public_db";
-// $db['live-hcg_write']['password'] = "jage0katt";
-// $db['live-hcg_write']['database'] = "hcgpublic_live";
-// $db['live-hcg_write']['dbdriver'] = "mysql";
-// $db['live-hcg_write']['dbprefix'] = "";
-// $db['live-hcg_write']['active_r'] = TRUE;
-// $db['live-hcg_write']['pconnect'] = TRUE;
-// $db['live-hcg_write']['db_debug'] = TRUE;
-// $db['live-hcg_write']['cache_on'] = FALSE;
-// $db['live-hcg_write']['cachedir'] = "";
-// $db['live-hcg_write']['char_set'] = "utf8";
-// $db['live-hcg_write']['dbcollat'] = "utf8_general_ci";
 
 // -----------------------------------------------------------------------
 // STAGE settings
 
 $db['stage-read']['hostname'] = "mysql-serv-master:3307";
 $db['stage-read']['username'] = "brewuser_stage";
-$db['stage-read']['password'] = "fr33tyr8";
+$db['stage-read']['password'] = "xxxxxx";
 $db['stage-read']['database'] = "coolbrew_stage";
 $db['stage-read']['dbdriver'] = "mysql";
 $db['stage-read']['dbprefix'] = "";
@@ -131,7 +103,7 @@ $db['stage-read']['dbcollat'] = "utf8_general_ci";
 
 $db['stage-write']['hostname'] = "mysql-serv-master:3307";
 $db['stage-write']['username'] = "brewuser_stage";
-$db['stage-write']['password'] = "fr33tyr8";
+$db['stage-write']['password'] = "xxxxxx";
 $db['stage-write']['database'] = "coolbrew_stage";
 $db['stage-write']['dbdriver'] = "mysql";
 $db['stage-write']['dbprefix'] = "";
@@ -143,40 +115,12 @@ $db['stage-write']['cachedir'] = "";
 $db['stage-write']['char_set'] = "utf8";
 $db['stage-write']['dbcollat'] = "utf8_general_ci";
 
-// $db['stage-hcg_read']['hostname'] = "mysql-serv-master:3307";
-// $db['stage-hcg_read']['username'] = "public_db";
-// $db['stage-hcg_read']['password'] = "jage0katt";
-// $db['stage-hcg_read']['database'] = "hcgpublic_stage";
-// $db['stage-hcg_read']['dbdriver'] = "mysql";
-// $db['stage-hcg_read']['dbprefix'] = "";
-// $db['stage-hcg_read']['active_r'] = TRUE;
-// $db['stage-hcg_read']['pconnect'] = TRUE;
-// $db['stage-hcg_read']['db_debug'] = TRUE;
-// $db['stage-hcg_read']['cache_on'] = FALSE;
-// $db['stage-hcg_read']['cachedir'] = "";
-// $db['stage-hcg_read']['char_set'] = "utf8";
-// $db['stage-hcg_read']['dbcollat'] = "utf8_general_ci";
-// 
-// $db['stage-hcg_write']['hostname'] = "mysql-serv-master:3307";
-// $db['stage-hcg_write']['username'] = "public_db";
-// $db['stage-hcg_write']['password'] = "jage0katt";
-// $db['stage-hcg_write']['database'] = "hcgpublic_stage";
-// $db['stage-hcg_write']['dbdriver'] = "mysql";
-// $db['stage-hcg_write']['dbprefix'] = "";
-// $db['stage-hcg_write']['active_r'] = TRUE;
-// $db['stage-hcg_write']['pconnect'] = TRUE;
-// $db['stage-hcg_write']['db_debug'] = TRUE;
-// $db['stage-hcg_write']['cache_on'] = FALSE;
-// $db['stage-hcg_write']['cachedir'] = "";
-// $db['stage-hcg_write']['char_set'] = "utf8";
-// $db['stage-hcg_write']['dbcollat'] = "utf8_general_ci";
-
 // -----------------------------------------------------------------------
 // DEV settings
 
 $db['dev-read']['hostname'] = "bolwebdev1:3306";
 $db['dev-read']['username'] = "brewuser";
-$db['dev-read']['password'] = "fr33tyr8";
+$db['dev-read']['password'] = "xxxxxx";
 $db['dev-read']['database'] = "coolbrew";
 $db['dev-read']['dbdriver'] = "mysql";
 $db['dev-read']['dbprefix'] = "";
@@ -190,7 +134,7 @@ $db['dev-read']['dbcollat'] = "utf8_general_ci";
 
 $db['dev-write']['hostname'] = "bolwebdev1:3306";
 $db['dev-write']['username'] = "brewuser";
-$db['dev-write']['password'] = "fr33tyr8";
+$db['dev-write']['password'] = "xxxxxx";
 $db['dev-write']['database'] = "coolbrew";
 $db['dev-write']['dbdriver'] = "mysql";
 $db['dev-write']['dbprefix'] = "";
@@ -202,40 +146,12 @@ $db['dev-write']['cachedir'] = "";
 $db['dev-write']['char_set'] = "utf8";
 $db['dev-write']['dbcollat'] = "utf8_general_ci";
 
-// $db['dev-hcg_read']['hostname'] = "bolwebdev1:3306";
-// $db['dev-hcg_read']['username'] = "public_db";
-// $db['dev-hcg_read']['password'] = "jage0katt";
-// $db['dev-hcg_read']['database'] = "hcg_public";
-// $db['dev-hcg_read']['dbdriver'] = "mysql";
-// $db['dev-hcg_read']['dbprefix'] = "";
-// $db['dev-hcg_read']['active_r'] = TRUE;
-// $db['dev-hcg_read']['pconnect'] = TRUE;
-// $db['dev-hcg_read']['db_debug'] = TRUE;
-// $db['dev-hcg_read']['cache_on'] = FALSE;
-// $db['dev-hcg_read']['cachedir'] = "";
-// $db['dev-hcg_read']['char_set'] = "utf8";
-// $db['dev-hcg_read']['dbcollat'] = "utf8_general_ci";
-// 
-// $db['dev-hcg_write']['hostname'] = "bolwebdev1:3306";
-// $db['dev-hcg_write']['username'] = "public_db";
-// $db['dev-hcg_write']['password'] = "jage0katt";
-// $db['dev-hcg_write']['database'] = "hcg_public";
-// $db['dev-hcg_write']['dbdriver'] = "mysql";
-// $db['dev-hcg_write']['dbprefix'] = "";
-// $db['dev-hcg_write']['active_r'] = TRUE;
-// $db['dev-hcg_write']['pconnect'] = TRUE;
-// $db['dev-hcg_write']['db_debug'] = TRUE;
-// $db['dev-hcg_write']['cache_on'] = FALSE;
-// $db['dev-hcg_write']['cachedir'] = "";
-// $db['dev-hcg_write']['char_set'] = "utf8";
-// $db['dev-hcg_write']['dbcollat'] = "utf8_general_ci";
-
 // -----------------------------------------------------------------------
 // LOCAL settings
 
 $db['local-read']['hostname'] = "localhost:3306";
 $db['local-read']['username'] = "brewuser";
-$db['local-read']['password'] = "vfw_ufy3HFN!dpx8dev";
+$db['local-read']['password'] = "xxxxxx";
 $db['local-read']['database'] = "coolbrew";
 $db['local-read']['dbdriver'] = "mysql";
 $db['local-read']['dbprefix'] = "";
@@ -249,7 +165,7 @@ $db['local-read']['dbcollat'] = "utf8_general_ci";
 
 $db['local-write']['hostname'] = "localhost:3306";
 $db['local-write']['username'] = "brewuser";
-$db['local-write']['password'] = "vfw_ufy3HFN!dpx8dev";
+$db['local-write']['password'] = "xxxxxx";
 $db['local-write']['database'] = "coolbrew";
 $db['local-write']['dbdriver'] = "mysql";
 $db['local-write']['dbprefix'] = "";
@@ -261,34 +177,6 @@ $db['local-write']['cachedir'] = "";
 $db['local-write']['char_set'] = "utf8";
 $db['local-write']['dbcollat'] = "utf8_general_ci";
 
-// $db['local-hcg_read']['hostname'] = "localhost:3306";
-// $db['local-hcg_read']['username'] = "brewuser";
-// $db['local-hcg_read']['password'] = "vfw_ufy3HFN!dpx8dev";
-// $db['local-hcg_read']['database'] = "hcgpublic";
-// $db['local-hcg_read']['dbdriver'] = "mysql";
-// $db['local-hcg_read']['dbprefix'] = "";
-// $db['local-hcg_read']['active_r'] = TRUE;
-// $db['local-hcg_read']['pconnect'] = TRUE;
-// $db['local-hcg_read']['db_debug'] = TRUE;
-// $db['local-hcg_read']['cache_on'] = FALSE;
-// $db['local-hcg_read']['cachedir'] = "";
-// $db['local-hcg_read']['char_set'] = "utf8";
-// $db['local-hcg_read']['dbcollat'] = "utf8_general_ci";
-// 
-// $db['local-hcg_write']['hostname'] = "localhost:3306";
-// $db['local-hcg_write']['username'] = "brewuser";
-// $db['local-hcg_write']['password'] = "vfw_ufy3HFN!dpx8dev";
-// $db['local-hcg_write']['database'] = "hcgpublic";
-// $db['local-hcg_write']['dbdriver'] = "mysql";
-// $db['local-hcg_write']['dbprefix'] = "";
-// $db['local-hcg_write']['active_r'] = TRUE;
-// $db['local-hcg_write']['pconnect'] = TRUE;
-// $db['local-hcg_write']['db_debug'] = TRUE;
-// $db['local-hcg_write']['cache_on'] = FALSE;
-// $db['local-hcg_write']['cachedir'] = "";
-// $db['local-hcg_write']['char_set'] = "utf8";
-// $db['local-hcg_write']['dbcollat'] = "utf8_general_ci";
-
 
 // -----------------------------------------------------------------------
 // Assign values to the official names based on DB_SERVER_LEVEL
@@ -298,32 +186,24 @@ if (DB_SERVER_LEVEL == "live")
    $active_group = "write";
    $db['read'] = $db['live-read'];
    $db['write'] = $db['live-write'];
-   // $db['hcg_read'] = $db['live-hcg_read'];
-   // $db['hcg_write'] = $db['live-hcg_write'];
 }
 elseif (DB_SERVER_LEVEL == "stage")
 {
    $active_group = "write";
    $db['read'] = $db['stage-read'];
    $db['write'] = $db['stage-write'];
-   // $db['hcg_read'] = $db['stage-hcg_read'];
-   // $db['hcg_write'] = $db['stage-hcg_write'];
 }
 elseif (DB_SERVER_LEVEL == "dev")
 {  
    $active_group = "read";
    $db['read'] = $db['dev-read'];
    $db['write'] = $db['dev-write'];
-   // $db['hcg_read'] = $db['dev-hcg_read'];
-   // $db['hcg_write'] = $db['dev-hcg_write'];
 }
 else  // server_level == "local"
 {  
    $active_group = "read";
    $db['read'] = $db['local-read'];
    $db['write'] = $db['local-write'];
-   // $db['hcg_read'] = $db['local-hcg_read'];
-   // $db['hcg_write'] = $db['local-hcg_write'];
 }
 
 ?>
